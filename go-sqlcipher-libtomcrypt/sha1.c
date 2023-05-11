b6ae8ebe9990c605d85d0306f5c28a3ff00de5d7
Lukáš Tinkl <lukas@kde.org>
2023-05-11 16:44:12 +0200

    [tomcrypt] add a generic fallback impl from nayuki
    this is already a 2x speedup

--- go-sqlcipher-libtomcrypt/sha1.c
+++ go-sqlcipher-libtomcrypt/sha1.c
@@ -10,6 +10,10 @@
  */
 #include "tomcrypt.h"
 
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
 /**
   @file sha1.c
   LTC_SHA1 code by Tom St Denis 
@@ -17,6 +21,44 @@
 
 
 #ifdef LTC_SHA1
+
+// -> BEGIN arm8 intrinsics block
+#if defined(__arm__) || defined(__aarch32__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM)
+# if defined(__GNUC__)
+#  include <stdint.h>
+# endif
+# if defined(__ARM_NEON)|| defined(_MSC_VER) || defined(__GNUC__)
+#  include <arm_neon.h>
+# endif
+/* GCC and LLVM Clang, but not Apple Clang */
+# if defined(__GNUC__) && !defined(__apple_build_version__)
+#  if defined(__ARM_ACLE) || defined(__ARM_FEATURE_CRYPTO)
+#   include <arm_acle.h>
+#  endif
+# endif
+#define SHA1_TARGET_ARM 1
+// -> END arm8 intrinsics block
+// -> BEGIN x86_64 intrinsics block
+#elif defined(__x86_64__) || defined(__SHA__)
+#if defined(__GNUC__) /* GCC and LLVM Clang */
+# include <x86intrin.h>
+#endif
+
+/* Microsoft supports Intel SHA ACLE extensions as of Visual Studio 2015 */
+#if defined(_MSC_VER)
+# include <immintrin.h>
+# define WIN32_LEAN_AND_MEAN
+# include <Windows.h>
+typedef UINT32 uint32_t;
+typedef UINT8 uint8_t;
+#endif
+#define SHA1_TARGET_X86 1
+#endif
+// -> END x86_64 intrinsics block
+
+#define LENGTH_SIZE 8  // In bytes
+#define BLOCK_LEN 64  // In bytes
+#define STATE_LEN 5  // In words
 
 const struct ltc_hash_descriptor sha1_desc =
 {
@@ -36,113 +78,144 @@ const struct ltc_hash_descriptor sha1_desc =
     NULL
 };
 
-#define F0(x,y,z)  (z ^ (x & (y ^ z)))
-#define F1(x,y,z)  (x ^ y ^ z)
-#define F2(x,y,z)  ((x & y) | (z & (x | y)))
-#define F3(x,y,z)  (x ^ y ^ z)
-
 #ifdef LTC_CLEAN_STACK
 static int _sha1_compress(hash_state *md, unsigned char *buf)
 #else
-static int  sha1_compress(hash_state *md, unsigned char *buf)
+static int sha1_compress(hash_state *md, unsigned char *buf)
 #endif
 {
-    ulong32 a,b,c,d,e,W[80],i;
-#ifdef LTC_SMALL_CODE
-    ulong32 t;
-#endif
-
-    /* copy the state into 512-bits into W[0..15] */
-    for (i = 0; i < 16; i++) {
-        LOAD32H(W[i], buf + (4*i));
-    }
-
-    /* copy state */
-    a = md->sha1.state[0];
-    b = md->sha1.state[1];
-    c = md->sha1.state[2];
-    d = md->sha1.state[3];
-    e = md->sha1.state[4];
-
-    /* expand it */
-    for (i = 16; i < 80; i++) {
-        W[i] = ROL(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1); 
-    }
-
-    /* compress */
-    /* round one */
-    #define FF0(a,b,c,d,e,i) e = (ROLc(a, 5) + F0(b,c,d) + e + W[i] + 0x5a827999UL); b = ROLc(b, 30);
-    #define FF1(a,b,c,d,e,i) e = (ROLc(a, 5) + F1(b,c,d) + e + W[i] + 0x6ed9eba1UL); b = ROLc(b, 30);
-    #define FF2(a,b,c,d,e,i) e = (ROLc(a, 5) + F2(b,c,d) + e + W[i] + 0x8f1bbcdcUL); b = ROLc(b, 30);
-    #define FF3(a,b,c,d,e,i) e = (ROLc(a, 5) + F3(b,c,d) + e + W[i] + 0xca62c1d6UL); b = ROLc(b, 30);
- 
-#ifdef LTC_SMALL_CODE
- 
-    for (i = 0; i < 20; ) {
-       FF0(a,b,c,d,e,i++); t = e; e = d; d = c; c = b; b = a; a = t;
-    }
-
-    for (; i < 40; ) {
-       FF1(a,b,c,d,e,i++); t = e; e = d; d = c; c = b; b = a; a = t;
-    }
-
-    for (; i < 60; ) {
-       FF2(a,b,c,d,e,i++); t = e; e = d; d = c; c = b; b = a; a = t;
-    }
-
-    for (; i < 80; ) {
-       FF3(a,b,c,d,e,i++); t = e; e = d; d = c; c = b; b = a; a = t;
-    }
-
+#if SHA1_TARGET_ARM
+    // FIXME
+#elif SHA1_TARGET_X86_FOO
+    // FIXME
 #else
+#define ROTL32(x, n)  (((0U + (x)) << (n)) | ((x) >> (32 - (n))))  // Assumes that x is uint32_t and 0 < n < 32
 
-    for (i = 0; i < 20; ) {
-       FF0(a,b,c,d,e,i++);
-       FF0(e,a,b,c,d,i++);
-       FF0(d,e,a,b,c,i++);
-       FF0(c,d,e,a,b,i++);
-       FF0(b,c,d,e,a,i++);
-    }
+#define LOADSCHEDULE(i)  \
+    schedule[i] = (uint32_t)buf[i * 4 + 0] << 24  \
+                  | (uint32_t)buf[i * 4 + 1] << 16  \
+                  | (uint32_t)buf[i * 4 + 2] <<  8  \
+                  | (uint32_t)buf[i * 4 + 3] <<  0;
 
-    /* round two */
-    for (; i < 40; )  { 
-       FF1(a,b,c,d,e,i++);
-       FF1(e,a,b,c,d,i++);
-       FF1(d,e,a,b,c,i++);
-       FF1(c,d,e,a,b,i++);
-       FF1(b,c,d,e,a,i++);
-    }
+#define SCHEDULE(i)  \
+    temp = schedule[(i - 3) & 0xF] ^ schedule[(i - 8) & 0xF] ^ schedule[(i - 14) & 0xF] ^ schedule[(i - 16) & 0xF];  \
+        schedule[i & 0xF] = ROTL32(temp, 1);
 
-    /* round three */
-    for (; i < 60; )  { 
-       FF2(a,b,c,d,e,i++);
-       FF2(e,a,b,c,d,i++);
-       FF2(d,e,a,b,c,i++);
-       FF2(c,d,e,a,b,i++);
-       FF2(b,c,d,e,a,i++);
-    }
+#define ROUND0a(a, b, c, d, e, i)  LOADSCHEDULE(i)  ROUNDTAIL(a, b, e, ((b & c) | (~b & d))         , i, 0x5A827999)
+#define ROUND0b(a, b, c, d, e, i)  SCHEDULE(i)      ROUNDTAIL(a, b, e, ((b & c) | (~b & d))         , i, 0x5A827999)
+#define ROUND1(a, b, c, d, e, i)   SCHEDULE(i)      ROUNDTAIL(a, b, e, (b ^ c ^ d)                  , i, 0x6ED9EBA1)
+#define ROUND2(a, b, c, d, e, i)   SCHEDULE(i)      ROUNDTAIL(a, b, e, ((b & c) ^ (b & d) ^ (c & d)), i, 0x8F1BBCDC)
+#define ROUND3(a, b, c, d, e, i)   SCHEDULE(i)      ROUNDTAIL(a, b, e, (b ^ c ^ d)                  , i, 0xCA62C1D6)
 
-    /* round four */
-    for (; i < 80; )  { 
-       FF3(a,b,c,d,e,i++);
-       FF3(e,a,b,c,d,i++);
-       FF3(d,e,a,b,c,i++);
-       FF3(c,d,e,a,b,i++);
-       FF3(b,c,d,e,a,i++);
-    }
+#define ROUNDTAIL(a, b, e, f, i, k)  \
+    e = 0U + e + ROTL32(a, 5) + f + UINT32_C(k) + schedule[i & 0xF];  \
+        b = ROTL32(b, 30);
+
+    uint32_t a = md->sha1.state[0];
+    uint32_t b = md->sha1.state[1];
+    uint32_t c = md->sha1.state[2];
+    uint32_t d = md->sha1.state[3];
+    uint32_t e = md->sha1.state[4];
+
+    uint32_t schedule[16];
+    uint32_t temp;
+    ROUND0a(a, b, c, d, e,  0)
+    ROUND0a(e, a, b, c, d,  1)
+    ROUND0a(d, e, a, b, c,  2)
+    ROUND0a(c, d, e, a, b,  3)
+    ROUND0a(b, c, d, e, a,  4)
+    ROUND0a(a, b, c, d, e,  5)
+    ROUND0a(e, a, b, c, d,  6)
+    ROUND0a(d, e, a, b, c,  7)
+    ROUND0a(c, d, e, a, b,  8)
+    ROUND0a(b, c, d, e, a,  9)
+    ROUND0a(a, b, c, d, e, 10)
+    ROUND0a(e, a, b, c, d, 11)
+    ROUND0a(d, e, a, b, c, 12)
+    ROUND0a(c, d, e, a, b, 13)
+    ROUND0a(b, c, d, e, a, 14)
+    ROUND0a(a, b, c, d, e, 15)
+    ROUND0b(e, a, b, c, d, 16)
+    ROUND0b(d, e, a, b, c, 17)
+    ROUND0b(c, d, e, a, b, 18)
+    ROUND0b(b, c, d, e, a, 19)
+    ROUND1(a, b, c, d, e, 20)
+    ROUND1(e, a, b, c, d, 21)
+    ROUND1(d, e, a, b, c, 22)
+    ROUND1(c, d, e, a, b, 23)
+    ROUND1(b, c, d, e, a, 24)
+    ROUND1(a, b, c, d, e, 25)
+    ROUND1(e, a, b, c, d, 26)
+    ROUND1(d, e, a, b, c, 27)
+    ROUND1(c, d, e, a, b, 28)
+    ROUND1(b, c, d, e, a, 29)
+    ROUND1(a, b, c, d, e, 30)
+    ROUND1(e, a, b, c, d, 31)
+    ROUND1(d, e, a, b, c, 32)
+    ROUND1(c, d, e, a, b, 33)
+    ROUND1(b, c, d, e, a, 34)
+    ROUND1(a, b, c, d, e, 35)
+    ROUND1(e, a, b, c, d, 36)
+    ROUND1(d, e, a, b, c, 37)
+    ROUND1(c, d, e, a, b, 38)
+    ROUND1(b, c, d, e, a, 39)
+    ROUND2(a, b, c, d, e, 40)
+    ROUND2(e, a, b, c, d, 41)
+    ROUND2(d, e, a, b, c, 42)
+    ROUND2(c, d, e, a, b, 43)
+    ROUND2(b, c, d, e, a, 44)
+    ROUND2(a, b, c, d, e, 45)
+    ROUND2(e, a, b, c, d, 46)
+    ROUND2(d, e, a, b, c, 47)
+    ROUND2(c, d, e, a, b, 48)
+    ROUND2(b, c, d, e, a, 49)
+    ROUND2(a, b, c, d, e, 50)
+    ROUND2(e, a, b, c, d, 51)
+    ROUND2(d, e, a, b, c, 52)
+    ROUND2(c, d, e, a, b, 53)
+    ROUND2(b, c, d, e, a, 54)
+    ROUND2(a, b, c, d, e, 55)
+    ROUND2(e, a, b, c, d, 56)
+    ROUND2(d, e, a, b, c, 57)
+    ROUND2(c, d, e, a, b, 58)
+    ROUND2(b, c, d, e, a, 59)
+    ROUND3(a, b, c, d, e, 60)
+    ROUND3(e, a, b, c, d, 61)
+    ROUND3(d, e, a, b, c, 62)
+    ROUND3(c, d, e, a, b, 63)
+    ROUND3(b, c, d, e, a, 64)
+    ROUND3(a, b, c, d, e, 65)
+    ROUND3(e, a, b, c, d, 66)
+    ROUND3(d, e, a, b, c, 67)
+    ROUND3(c, d, e, a, b, 68)
+    ROUND3(b, c, d, e, a, 69)
+    ROUND3(a, b, c, d, e, 70)
+    ROUND3(e, a, b, c, d, 71)
+    ROUND3(d, e, a, b, c, 72)
+    ROUND3(c, d, e, a, b, 73)
+    ROUND3(b, c, d, e, a, 74)
+    ROUND3(a, b, c, d, e, 75)
+    ROUND3(e, a, b, c, d, 76)
+    ROUND3(d, e, a, b, c, 77)
+    ROUND3(c, d, e, a, b, 78)
+    ROUND3(b, c, d, e, a, 79)
+
+    md->sha1.state[0] = 0U + md->sha1.state[0] + a;
+    md->sha1.state[1] = 0U + md->sha1.state[1] + b;
+    md->sha1.state[2] = 0U + md->sha1.state[2] + c;
+    md->sha1.state[3] = 0U + md->sha1.state[3] + d;
+    md->sha1.state[4] = 0U + md->sha1.state[4] + e;
+
+#undef ROTL32
+#undef LOADSCHEDULE
+#undef SCHEDULE
+#undef ROUND0a
+#undef ROUND0b
+#undef ROUND1
+#undef ROUND2
+#undef ROUND3
+#undef ROUNDTAIL
 #endif
-
-    #undef FF0
-    #undef FF1
-    #undef FF2
-    #undef FF3
-
-    /* store */
-    md->sha1.state[0] = md->sha1.state[0] + a;
-    md->sha1.state[1] = md->sha1.state[1] + b;
-    md->sha1.state[2] = md->sha1.state[2] + c;
-    md->sha1.state[3] = md->sha1.state[3] + d;
-    md->sha1.state[4] = md->sha1.state[4] + e;
 
     return CRYPT_OK;
 }
